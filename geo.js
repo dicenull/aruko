cdd1f3b4b1937e635a8309dfb7360f40a1a8b8f0
dice <dicenull.dev@gmail.com>
2019-10-04 16:15:45 +0900

    内容を整理

--- geo.js
+++ geo.js
@@ -1,28 +1,33 @@
 let dist;
 let first_dist;
+let end_pos;
+
+window.onload = function () {
+    let res = JSON.parse(req.response);
+    let bindings = res.results.bindings;
+
+    let base = bindings[Math.random() * Object.keys(bindings).length | 0];
+    console.log(base);
+    end_pos = { latitude: base.lat.value, longitude: base.lng.value };
+};
 
 function success(pos) {
     let crd = pos.coords;
 
     let start_pos = { latitude: crd.latitude, longitude: crd.longitude };
-    let end_pos;
 
     var req = new XMLHttpRequest();
-    req.open("GET", "http://localhost:3000/api/test?latitude=" + crd.latitude + "&longitude=" + crd.longitude + "&time=" + 30, true);
+    req.open("GET", "https://ea9e0fcb.ngrok.io/api/test?latitude=" + crd.latitude + "&longitude=" + crd.longitude + "&time=" + 30, true);
 
     req.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE) {
-            let res = JSON.parse(req.response);
-            let base = res.target[0];
-            end_pos = { latitude: base.t_latitude, longitude: base.t_longitude };
-            console.log(end_pos);
-
             dist = window.geolib.getDistance(start_pos, end_pos);
 
             if (first_dist === undefined) {
                 first_dist = dist;
             }
 
+            // 緯度経度と距離をデバック表示
             let crd_div = document.getElementById("coords");
             let dist_div = document.getElementById("dist");
 
